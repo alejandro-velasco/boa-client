5f696b76ca5f49e50194737ff38a7a4736f02a15
Alejandro Velasco <alejandro.velasco1423@gmail.com>
2023-07-23 19:14:42 -0700

    separating script def from stage

--- boa_client/jobs.py
+++ boa_client/jobs.py
@@ -17,12 +17,17 @@ class BoaJob:
         stages = self.file['stages']
 
         for stage_name, stage_spec in stages.items():
-            self.execute_stage(stage_name, stage_spec)
+            self.execute_stage(stage_name=stage_name, stage_spec=stage_spec)
     
     def execute_stage(self, stage_name, stage_spec):
         logging.info(f'Executing stage "{stage_name}"')
 
-        for step in stage_spec['script']:
+        for step_name, step_spec in stage_spec.items():
+            if step_name == 'script':
+                self.execute_script(step_spec)
+
+    def execute_script(self, script):
+        for step in script:
             logging.debug(f'\n{step}')
             p = subprocess.Popen([step], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
     
