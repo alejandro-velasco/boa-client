087bd8443447b76ba165569a9be75a369577e330
alejandro-velasco <alejandro.velasco1423@gmail.com>
2023-07-27 21:57:41 -0700

    fixing indentation

--- boa_client/scm.py
+++ boa_client/scm.py
@@ -3,27 +3,26 @@ import logging
 import rich
     
 def clone(url, submodules=False, branch="", name=""):
+    cmd = ["git", "clone", url]
+    if submodules:
+       cmd.append("--recurse-submodules")
+    if branch:
+        cmd = cmd + ["--branch", branch]
+    if name:
+        cmd.append(name)
 
-        cmd = ["git", "clone", url]
-        if submodules:
-           cmd.append("--recurse-submodules")
-        if branch:
-            cmd = cmd + ["--branch", branch]
-        if name:
-            cmd.append(name)
+    logging.info(f"Cloning {url}")
+    logging.debug(" ".join(cmd))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    
+    output, errors = p.communicate()
 
-        logging.info(f"Cloning {url}")
-        logging.debug(" ".join(cmd))
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
- 
-        output, errors = p.communicate()
+    if p.returncode != 0:
+        # handle error
+        logging.error(errors)
+        raise SystemExit(1)
 
-        if p.returncode != 0:
-            # handle error
-            logging.error(errors)
-            raise SystemExit(1)
-
-        logging.info(f'{output}')
+    logging.info(f'{output}')
 
 
     
